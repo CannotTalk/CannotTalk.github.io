6ffc1544512f43040a36e020d14dd0960192ccc8
CannotTalk <168893556+CannotTalk@users.noreply.github.com>
2024-09-03 23:57:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@
 	let random = Math.trunc(Math.random()*Math.pow(10,Math.trunc(Math.random()*10)));
 	document.getElementById("rand").innerHTML = random;
 	if(random == 0){
-		window.alert("おめでとう!\n君が初めてのサイト訪問者だ!再読み込みするね!")
+		window.alert("おめでとう!\n君が初めてのサイト訪問者だ!再読み込みするね!");
+		window.location.reload();
 	}
 }
 
